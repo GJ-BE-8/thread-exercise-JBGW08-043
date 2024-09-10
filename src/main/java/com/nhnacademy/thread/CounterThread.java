230e31bf40df76e1d84da5c6a9f93f784899065f
조승주 <tmdwn3318@gmail.com>
2024-09-10 13:30:36 +0900

    1-2 todo 실습

--- src/main/java/com/nhnacademy/thread/CounterThread.java
+++ src/main/java/com/nhnacademy/thread/CounterThread.java
@@ -18,16 +18,20 @@ import java.util.Objects;
 
 @Slf4j
 //TODO#1 CounterThread는 Thread를 상속 합니다.
-public class CounterThread {
+public class CounterThread extends Thread{
     private final long countMaxSize;
 
     private long count;
 
     public CounterThread(String name, long countMaxSize) {
         //TODO#2 name <-- null 이거나 공백 문자열이면 IllegalArgumentException이 발생 합니다.
-
+        if(Objects.isNull(name) || name.isBlank()){
+            throw new IllegalArgumentException();
+        }
         //TODO#3 countMaxSize <=0 이면 IllegalArgumentException이 발생 합니다.
-
+        if(countMaxSize <= 0){
+            throw new IllegalArgumentException();
+        }
 
         this.setName(name);
         this.countMaxSize = countMaxSize;
@@ -46,6 +50,15 @@ public class CounterThread {
          */
 
         do {
+            try{
+            Thread.sleep(1000);
+
+            }catch (InterruptedException e){
+                log.error("error: {}", e);
+            }
+            count++;
+            System.out.println("thread: " + this.getName() + ", count: " + count);
+
 
         }while (count<countMaxSize);
     }
